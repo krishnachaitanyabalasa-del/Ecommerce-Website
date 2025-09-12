9b9f8634319fbf501d85e44e20360c8bd2358c70
Krishna Chaitanya Balasa <your-email@example.com>
2025-09-12 23:04:40 +0530

    seperate into smaller components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 import {Routes,Route} from 'react-router';
-import { HomePage } from './pages/HomePage';
-import {CheckoutPage} from './pages/CheckoutPage';
-import { OrdersPage } from './pages/Orderspage';
+import { HomePage } from './pages/home/HomePage';
+import {CheckoutPage} from './pages/checkout/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
 import {TrackingPage} from './pages/TrackingPage';
 import { useState,useEffect } from 'react';
 import axios from 'axios';
---
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect,useState } from "react";
+import { Header } from "../../components/header";
+import { ProductsGrid } from "./ProductsGrid";
+import "./HomePage.css";
+export function HomePage({cart}) {
+  const [products,setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("/api/products").then((response) => {
+      setProducts(response.data);
+    });
+    
+  },[]); //[] - run once when component mounts
+
+  return (
+    <>
+      <title>Ecommerce Project</title>
+      <Header cart={cart}/>
+
+      <div className="home-page">
+        <ProductsGrid products={products}/>
+      </div>
+    </>
+  );
+}
