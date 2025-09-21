102b337bd4792e523fe4ab0584162fe97fe06248
Krishna Chaitanya Balasa <your-email@example.com>
2025-09-21 16:54:24 +0530

    Convert e commerce project to typescript

---
+++ old-projects/ecommerce-project-js/src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./checkout-header.css";
+import "./CheckoutPage.css";
+import { OrderSummary } from "./OrderSummary.jsx";
+import { PaymentSummary } from "./PaymentSummary.jsx";
+export function CheckoutPage({ cart,loadCart }) {
+  const [deliveryOptions, setDeliveryOptions] = useState([]);
+  const [paymentSummary, setPaymentSummary] = useState(null);
+
+  useEffect(() => {
+    const fetchCheckOutData = async () =>{
+      let response = await axios
+      .get(
+        "/api/delivery-options?expand=estimatedDeliveryTime"
+      );
+      setDeliveryOptions(response.data);
+      
+    response = await axios.get("/api/payment-summary");
+    setPaymentSummary(response.data);
+    
+    };
+    fetchCheckOutData();
+  }, [cart]); //[cart] this is dependency when anything change in a cart this will run.
+  return (
+    <>
+      <title>Checkout</title>
+      <div className="checkout-header">
+        <div className="header-content">
+          <div className="checkout-header-left-section">
+            <a href="/">
+              <img className="logo" src="images/logo.png" />
+              <img className="mobile-logo" src="images/mobile-logo.png" />
+            </a>
+          </div>
+
+          <div className="checkout-header-middle-section">
+            Checkout (
+            <a className="return-to-home-link" href="/">
+              3 items
+            </a>
+            )
+          </div>
+
+          <div className="checkout-header-right-section">
+            <img src="images/icons/checkout-lock-icon.png" />
+          </div>
+        </div>
+      </div>
+
+      <div className="checkout-page">
+        <div className="page-title">Review your order</div>
+
+        <div className="checkout-grid">
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
+
+          <PaymentSummary paymentSummary={paymentSummary} 
+          loadCart={loadCart}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
